a645c3b88b137071f83f6bc1654c58535a6f2d14
Habtamu Tesfaye <habtamutesfaye.com@gmail.com>
2025-07-25 21:05:26 +0300

    refactor: added RolePermissionMiddleware to middleware

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -75,7 +75,7 @@ class OffensiveLanguageMiddleware(MiddlewareMixin):
         return ip
 
 
-class RolePermissionMiddleware(MiddlewareMixin):
+class RolepermissionMiddleware(MiddlewareMixin):
     def __init__(self, get_response=None):
         super().__init__(get_response)
 
